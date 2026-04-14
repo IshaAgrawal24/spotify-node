2b953a1af6b288000d28f00c9d1d605d39b0c454
IshaAgrawal24 <agrawalisha33@gmail.com>
2026-04-15 00:44:40 +0530

    Create login and upload-music API

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const authController = require('../controllers/auth.controller');
+const express = require("express");
+const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-// Register API 
-router.post('/register', authController.registerUser);
+// Register API
+router.post("/register", authController.registerUser);
+// Login API
+router.post("/login", authController.loginUser);
 
 module.exports = router;
