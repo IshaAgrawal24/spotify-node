0c71f436058c306b7a13074f7ad21761020abf6a
IshaAgrawal24 <agrawalisha33@gmail.com>
2026-04-15 21:24:25 +0530

    Optimise the APIs to move duplicate code to middleware

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -4,82 +4,43 @@ const musicModel = require("../models/music.model");
 const albumModel = require("../models/album.model");
 
 async function createMusic(req, res) {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({
-      return_status: 401,
-      message: "User is unauthorised.",
-    });
-  }
+  const { title } = req.body;
+  const file = req.file;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role !== "artist") {
-      return res.status(403).json({
-        status_code: 403,
-        return_message: "You don't have an access to create music.",
-      });
-    }
+  const musicFile = await uploadFile(file.buffer.toString("base64"));
 
-    const { title } = req.body;
-    const file = req.file;
+  const music = await musicModel.create({
+    uri: musicFile.url,
+    title: title,
+    artist: req.user.id,
+  });
 
-    const musicFile = await uploadFile(file.buffer.toString("base64"));
-
-    const music = await musicModel.create({
-      uri: musicFile.url,
-      title: title,
-      artist: decoded.id,
-    });
-
-    res.status(201).json({
-      status_code: 201,
-      return_message: "New Music is created successfully.",
-      music: {
-        id: music._id,
-        title: music.title,
-        file: music.uri,
-        artist: music.artist,
-      },
-    });
-  } catch (error) {
-    console.log("Error::", error);
-    return res.status(401).json("Unauthorized");
-  }
+  res.status(201).json({
+    status_code: 201,
+    return_message: "New Music is created successfully.",
+    music: {
+      id: music._id,
+      title: music.title,
+      file: music.uri,
+      artist: music.artist,
+    },
+  });
 }
 
 async function createAlbum(req, res) {
-  const token = req.cookies.token;
+  const { title, musics } = req.body;
 
-  if (!token) {
-    return res.status(401).json({ message: "unauthorized" });
-  }
+  const album = await albumModel.create({
+    title: title,
+    musics: musics,
+    artist: req.user.id,
+  });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (decoded.role != "artist") {
-      return res.status(403).json({ message: "Sorry, YOu do not have as access to create an album." });
-    }
-
-    const {title, musics} = req.body;
-
-    const album = await albumModel.create({
-      title: title,
-      musics: musics,
-      artist: decoded.id
-    })
-
-    res.status(201).json({
-      status_code: 201,
-      return_message: "Alnbum created successfully.",
-      album: album
-    })
-
-  } catch (error) {
-    console.log("Album Error:", error);
-    return res.status(401).json({ message: "unauthorized" });
-  }
+  res.status(201).json({
+    status_code: 201,
+    return_message: "Album created successfully.",
+    album: album,
+  });
 }
 
 module.exports = { createMusic, createAlbum };
--- src/routes/music.route.js
+++ src/routes/music.route.js
@@ -2,17 +2,25 @@ const express = require("express");
 const multer = require("multer");
 
 const musicController = require("../controllers/music.controller");
+const authMiddleWare = require("../middlewares/auth.middleware");
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 
+// Create Music POST API 
 router.post(
   "/upload-music",
+  authMiddleWare.authArtist,
   upload.single("music"),
   musicController.createMusic,
 );
 
-router.post('/upload-album', musicController.createAlbum)
+// Create Album POST API 
+router.post(
+  "/upload-album",
+  authMiddleWare.authArtist,
+  musicController.createAlbum,
+);
 
 module.exports = router;
