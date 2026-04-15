5bfc8c58213b49ae6908d056bc04d79566c07a86
IshaAgrawal24 <agrawalisha33@gmail.com>
2026-04-16 00:35:27 +0530

    Implement get API & some functions

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,7 +14,7 @@ async function authArtist(req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
-    
+
     if (decoded.role != "artist") {
       return res.status(403).json({
         status_code: 401,
@@ -32,4 +32,34 @@ async function authArtist(req, res, next) {
   }
 }
 
-module.exports = { authArtist };
+async function authUser(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      status_code: 401,
+      return_message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role != "user" && decoded.role != "artist") {
+      return res.status(403).json({
+        status_code: 401,
+        return_message: "Unauthorized",
+      });
+    }
+
+    next();
+  } catch (err) {
+    console.log("AuthUser Middleware Error", error);
+    return res.status(401).json({
+      status_code: 401,
+      return_message: "Unauthorized",
+    });
+  }
+}
+
+module.exports = { authArtist, authUser };
